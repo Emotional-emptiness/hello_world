6ba10b2745a9b9a8f1da3eaa4fe696b4b4889b76
Serg <moon.linexx@gmail.com>
2023-04-02 18:44:42 +0300

    Changed main.java

--- src/main/java/helloworld/java/Main.java
+++ src/main/java/helloworld/java/Main.java
@@ -3,5 +3,6 @@ package helloworld.java;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello GOIT world!");
+        System.out.println("this is our second commit");
     }
 }
